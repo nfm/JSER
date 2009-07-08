ef03cead84fb229204cf07078271fcff1cfb72ce
nfm <nfm@deagle.(none)>
2009-07-08 15:34:15 +1000

    Reverted id's on buttons to their tag names.

--- init.js
+++ init.js
@@ -1,8 +1,8 @@
 //<![CDATA[
 
-$('bold').observe('click', toggleBold);
-$('italic').observe('click', toggleItalic);
-$('underline').observe('click', toggleUnderline);
+$('b').observe('click', toggleBold);
+$('i').observe('click', toggleItalic);
+$('u').observe('click', toggleUnderline);
 document.observe('keydown', processKeyDown);
 document.observe('keypress', processKeyPress);
 
