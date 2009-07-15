301e608b40996b102c90f25fb6124cf34c6564ed
Nicholas Firth-McCoy <nfm@scout.(none)>
2009-07-15 15:29:10 +1000

    Added initial functionality for focussing on the editor area.

--- init.js
+++ init.js
@@ -1,10 +1,19 @@
 //<![CDATA[
 
+// Observe buttons for mouse click
 $('b').observe('click', toggleBold);
 $('i').observe('click', toggleItalic);
 $('u').observe('click', toggleUnderline);
+
+// Observe document for keyboard activity
 document.observe('keydown', processKeyDown);
 document.observe('keypress', processKeyPress);
+
+// Observe document and editor for 'focus' to determine visibility of cursor
+document.observe('click', setCursorHidden);
+document.observe('click', stopCursorTimer);
+$('editor').observe('click', restartCursorTimer);
+$('editor').observe('click', Event.stop);
 
 startCursorTimer();
 
